fdfcaa44afa8349e477d41b248b9f4fc67de59ad
Joao Marcelo <marcelooliveira004@outlook.com>
2024-09-19 01:04:03 -0300

    feat: generate book model and migration

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import authMiddleware from "../middlewares/auth";
 import UserController from "../controllers/user";
 import CategoryController from "../controllers/category";
 import AuthorController from "../controllers/author";
+import BookController from "../controllers/book";
 
 const routes = new Router();
 // ______ unauthenticated routes ____________
@@ -16,4 +17,6 @@ routes.get("/user", UserController.get);
 routes.get("/category", CategoryController.getAll);
 routes.post("/author", AuthorController.create);
 routes.get("/author", AuthorController.getAll);
+routes.post("/book", BookController.create);
+routes.get("/book", BookController.findAll);
 export default routes;
