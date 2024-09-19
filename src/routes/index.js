7667c21c8f41e82bddb9dc70379d04996274e011
Joao Marcelo <marcelooliveira004@outlook.com>
2024-09-18 22:31:52 -0300

    feat: add authors and categories models and migrations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth";
 import UserController from "../controllers/user";
+import CategoryController from "../controllers/category";
+import AuthorController from "../controllers/author";
 
 const routes = new Router();
 // ______ unauthenticated routes ____________
@@ -11,4 +13,7 @@ routes.post("/reset-password", UserController.resetPassword);
 // ______ authenticated routes ____________
 routes.use(authMiddleware);
 routes.get("/user", UserController.get);
+routes.get("/category", CategoryController.getAll);
+routes.post("/author", AuthorController.create);
+routes.get("/author", AuthorController.getAll);
 export default routes;
